7a7d393d9b69a42709a4eb6bb011951a1ee79187
wiley68 <ilko.iv@gmail.com>
2023-04-01 16:46:00 +0300

    Update DirectMessages.vue

--- src/components/DirectMessages.vue
+++ src/components/DirectMessages.vue
@@ -59,7 +59,7 @@ const getMessagesByUserLength = (user_id) => {
 
 <template>
 	<div class="mt-4">
-		<div class="text-xs font-semibold text-gray-400 uppercase mb-3">
+		<div class="text-lg font-semibold text-gray-400 uppercase mb-3">
 			<input
 				type="checkbox"
 				class="mr-2 w-5 h-5"
@@ -70,11 +70,11 @@ const getMessagesByUserLength = (user_id) => {
 		</div>
 		<ul class="mb-6">
 			<li
-				class="flex flex-row justify-center items-center -mx-2 mb-1"
+				class="flex flex-row justify-left items-center -mx-2 mb-1"
 				v-for="user in getUsers"
 				:key="user.id"
 			>
-				<input type="checkbox" class="mr-2 w-4 h-4" v-model="user.checked" />
+				<input type="checkbox" class="mr-2 w-5 h-5" v-model="user.checked" />
 				<button
 					class="flex items-center justify-between w-full p-2 rounded hover:bg-indigo-100"
 					:class="user.id == store.state.current_user_id ? 'bg-indigo-100' : ''"
@@ -83,12 +83,12 @@ const getMessagesByUserLength = (user_id) => {
 					<div class="flex items-center truncate">
 						<img
 							v-if="user.user_gravatar != ''"
-							class="w-7 h-7 rounded-full mr-2"
+							class="w-10 h-10 rounded-full mr-2"
 							:src="'https://www.gravatar.com/avatar/' + user.user_gravatar"
 						/>
 						<svg
 							v-if="user.user_gravatar == ''"
-							class="w-7 h-7 mr-2"
+							class="w-10 h-10 mr-2"
 							viewBox="0 0 24 24"
 						>
 							<path
@@ -97,9 +97,9 @@ const getMessagesByUserLength = (user_id) => {
 							/>
 						</svg>
 						<div class="truncate">
-							<span class="text-sm font-medium text-gray-800"
+							<span class="font-medium text-gray-800"
 								>{{ user.username }}
-								<span class="text-xs text-gray-400"
+								<span class="text-sm text-gray-400"
 									>({{ user.dlaznost }})</span
 								></span
 							>
@@ -108,7 +108,7 @@ const getMessagesByUserLength = (user_id) => {
 					<div class="flex items-center ml-2">
 						<svg
 							v-if="getMessagesByUserLength(user.id) == 0"
-							class="w-3 h-3 shrink-0 fill-current text-gray-400"
+							class="w-5 h-5 shrink-0 fill-current text-gray-400"
 							viewBox="0 0 12 12"
 						>
 							<path
@@ -117,7 +117,7 @@ const getMessagesByUserLength = (user_id) => {
 						</svg>
 						<div
 							v-if="getMessagesByUserLength(user.id) > 0"
-							class="text-xs inline-flex font-medium bg-indigo-400 text-white rounded-full text-center leading-5 px-2"
+							class="text-sm inline-flex font-medium bg-indigo-400 text-white rounded-full text-center leading-5 px-4"
 						>
 							{{ getMessagesByUserLength(user.id) }}
 						</div>
