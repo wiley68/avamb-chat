b8921c8e0dfb76f014b38466d054af779fbb9b07
wiley68 <ilko.iv@gmail.com>
2023-04-17 10:22:18 +0300

    Update MessagesBody.vue

--- src/components/MessagesBody.vue
+++ src/components/MessagesBody.vue
@@ -116,7 +116,7 @@ const deleteMessage = () => {
 			:key="message.id"
 			class="flex items-center justify-start mb-4 last:mb-0"
 		>
-			<img
+			<!-- <img
 				v-if="store.getUserById(message.from_user_id).user_gravatar != ''"
 				class="w-12 h-12 rounded-full mr-2"
 				:src="
@@ -133,13 +133,21 @@ const deleteMessage = () => {
 					fill="currentColor"
 					d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z"
 				/>
+			</svg> -->
+			<svg
+				class="w-10 h-10 shrink-0 fill-current text-gray-400 mr-2"
+				viewBox="0 0 12 12"
+			>
+				<path
+					d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z"
+				/>
 			</svg>
 			<div>
 				<div class="flex items-center">
-					<span class="text-lg text-red-600 font-bold">{{
+					<span class="text-xl text-red-600 font-bold">{{
 						message.offer_idnomber
 					}}</span>
-					<span class="ml-2 text-base text-gray-500 font-medium">{{
+					<span class="ml-2 text-lg text-gray-500 font-medium">{{
 						store.getUserById(message.from_user_id).username
 					}}</span
 					><svg
@@ -150,7 +158,7 @@ const deleteMessage = () => {
 						<path
 							d="M5.59,7.41L7,6L13,12L7,18L5.59,16.59L10.17,12L5.59,7.41M11.59,7.41L13,6L19,12L13,18L11.59,16.59L16.17,12L11.59,7.41Z"
 						/></svg
-					><span class="ml-2 text-base text-gray-500 font-medium">{{
+					><span class="ml-2 text-lg text-gray-500 font-medium">{{
 						store.getUserById(message.to_user_id).username
 					}}</span>
 				</div>
@@ -165,33 +173,25 @@ const deleteMessage = () => {
 					{{ message.body }}
 				</div>
 				<div class="flex items-center">
-					<div class="flex-grow text-sm text-gray-500 font-medium">
+					<div class="flex-grow text-base text-gray-500 font-medium">
 						{{ formatDateTime(message.created_at) }}
 					</div>
-					<svg
-						class="w-4 h-4 shrink-0 fill-current text-gray-400"
-						viewBox="0 0 12 12"
-					>
-						<path
-							d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z"
-						/>
-					</svg>
-					<button
-						@click.stop="deleteMessageCheck(message.id)"
-						title="Изтрий избраното съобщение"
-					>
-						<svg
-							class="w-8 h-8 text-red-400 hover:text-red-600"
-							viewBox="0 0 24 24"
-						>
-							<path
-								fill="currentColor"
-								d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"
-							/>
-						</svg>
-					</button>
 				</div>
 			</div>
+			<button
+				@click.stop="deleteMessageCheck(message.id)"
+				title="Изтрий избраното съобщение"
+			>
+				<svg
+					class="w-10 h-10 text-red-400 hover:text-red-600 ml-2"
+					viewBox="0 0 24 24"
+				>
+					<path
+						fill="currentColor"
+						d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"
+					/>
+				</svg>
+			</button>
 		</div>
 		<div
 			v-for="message in messages_today"
@@ -203,7 +203,7 @@ const deleteMessage = () => {
 					: 'justify-start'
 			"
 		>
-			<img
+			<!-- <img
 				v-if="store.getUserById(message.from_user_id).user_gravatar != ''"
 				class="w-12 h-12 rounded-full mr-2"
 				:src="
@@ -220,13 +220,24 @@ const deleteMessage = () => {
 					fill="currentColor"
 					d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z"
 				/>
-			</svg>
+			</svg> -->
+			<button @click.stop="changeStatus(message.id)" title="Промени статуса">
+				<svg
+					class="w-10 h-10 shrink-0 fill-current mr-2"
+					:class="message.status == 0 ? 'text-gray-400' : 'text-green-400'"
+					viewBox="0 0 12 12"
+				>
+					<path
+						d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z"
+					/>
+				</svg>
+			</button>
 			<div>
 				<div class="flex items-center">
-					<span class="text-lg text-red-600 font-bold">{{
+					<span class="text-xl text-red-600 font-bold">{{
 						message.offer_idnomber
 					}}</span>
-					<span class="ml-2 text-base text-gray-500 font-medium">{{
+					<span class="ml-2 text-lg text-gray-500 font-medium">{{
 						store.getUserById(message.from_user_id).username
 					}}</span>
 				</div>
@@ -241,43 +252,29 @@ const deleteMessage = () => {
 					{{ message.body }}
 				</div>
 				<div class="flex items-center">
-					<div class="flex-grow text-sm text-gray-500 font-medium">
+					<div class="flex-grow text-base text-gray-500 font-medium">
 						{{ formatDateTime(message.created_at) }}
 					</div>
-					<button
-						@click.stop="changeStatus(message.id)"
-						title="Промени статуса"
-					>
-						<svg
-							class="w-4 h-4 shrink-0 fill-current"
-							:class="message.status == 0 ? 'text-gray-400' : 'text-green-400'"
-							viewBox="0 0 12 12"
-						>
-							<path
-								d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z"
-							/>
-						</svg>
-					</button>
-					<button
-						@click.stop="deleteMessageCheck(message.id)"
-						title="Изтрий избраното съобщение"
-					>
-						<svg
-							class="w-8 h-8 text-red-400 hover:text-red-600"
-							viewBox="0 0 24 24"
-						>
-							<path
-								fill="currentColor"
-								d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"
-							/>
-						</svg>
-					</button>
 				</div>
 			</div>
+			<button
+				@click.stop="deleteMessageCheck(message.id)"
+				title="Изтрий избраното съобщение"
+			>
+				<svg
+					class="w-10 h-10 text-red-400 hover:text-red-600 ml-2"
+					viewBox="0 0 24 24"
+				>
+					<path
+						fill="currentColor"
+						d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"
+					/>
+				</svg>
+			</button>
 		</div>
 		<div class="flex justify-center">
 			<div
-				class="inline-flex items-center justify-center text-sm font-medium px-2.5 py-1 bg-white border border-gray-200 rounded-full my-5"
+				class="inline-flex items-center justify-center text-base font-medium px-2.5 py-1 bg-white border border-gray-200 rounded-full my-5"
 			>
 				{{ today() }}
 			</div>
@@ -292,7 +289,7 @@ const deleteMessage = () => {
 					: 'justify-start'
 			"
 		>
-			<img
+			<!-- <img
 				v-if="store.getUserById(message_old.from_user_id).user_gravatar != ''"
 				class="w-12 h-12 rounded-full mr-2"
 				:src="
@@ -309,13 +306,27 @@ const deleteMessage = () => {
 					fill="currentColor"
 					d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z"
 				/>
-			</svg>
+			</svg> -->
+			<button
+				@click.stop="changeStatus(message_old.id)"
+				title="Промени статуса"
+			>
+				<svg
+					class="w-10 h-10 shrink-0 fill-current text-gray-400 mr-2"
+					:class="message_old.status == 0 ? 'text-gray-400' : 'text-green-400'"
+					viewBox="0 0 12 12"
+				>
+					<path
+						d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z"
+					/>
+				</svg>
+			</button>
 			<div>
 				<div class="flex items-center">
-					<span class="text-lg text-red-600 font-bold">{{
+					<span class="text-xl text-red-600 font-bold">{{
 						message_old.offer_idnomber
 					}}</span>
-					<div class="ml-2 text-base text-gray-500 font-medium">
+					<div class="ml-2 text-lg text-gray-500 font-medium">
 						{{ store.getUserById(message_old.from_user_id).username }}
 					</div>
 				</div>
@@ -330,41 +341,25 @@ const deleteMessage = () => {
 					{{ message_old.body }}
 				</div>
 				<div class="flex items-center">
-					<div class="flex-grow text-sm text-gray-500 font-medium">
+					<div class="flex-grow text-base text-gray-500 font-medium">
 						{{ formatDateTime(message_old.created_at) }}
 					</div>
-					<button
-						@click.stop="changeStatus(message_old.id)"
-						title="Промени статуса"
-					>
-						<svg
-							class="w-4 h-4 shrink-0 fill-current text-gray-400"
-							:class="
-								message_old.status == 0 ? 'text-gray-400' : 'text-green-400'
-							"
-							viewBox="0 0 12 12"
-						>
-							<path
-								d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z"
-							/>
-						</svg>
-					</button>
-					<button
-						@click.stop="deleteMessageCheck(message_old.id)"
-						itle="Изтрий избраното съобщение"
-					>
-						<svg
-							class="w-8 h-8 text-red-400 hover:text-red-600"
-							viewBox="0 0 24 24"
-						>
-							<path
-								fill="currentColor"
-								d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"
-							/>
-						</svg>
-					</button>
 				</div>
 			</div>
+			<button
+				@click.stop="deleteMessageCheck(message_old.id)"
+				itle="Изтрий избраното съобщение"
+			>
+				<svg
+					class="w-10 h-10 text-red-400 hover:text-red-600 ml-2"
+					viewBox="0 0 24 24"
+				>
+					<path
+						fill="currentColor"
+						d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z"
+					/>
+				</svg>
+			</button>
 		</div>
 		<ModalBlank
 			id="danger-modal"
